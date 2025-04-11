7440e6da40b8bbaba3f1ddb827415d870138d93a
Santosh Linux <santosh09@gmail.com>
2025-04-11 11:39:44 +0000

    few more changes in falco installaiton

--- falco-installation/data.tf
+++ falco-installation/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "eks" {
     backend = "local"
     config = {
-        path = "${path.module}/../eks/terraform.tfstate"
+        path = "${path.module}/../terraform.tfstate"
     }
     
 }
--- falco-installation/provider.tf
+++ falco-installation/provider.tf
@@ -9,10 +9,10 @@ terraform {
           source  = "hashicorp/kubernetes"
           version = "~> 2.36"
         }
-        #   helm = {
-        #   source  = "hashicorp/helm"
-        #   version = "~> 2.17"
-        # }
+          helm = {
+          source  = "hashicorp/helm"
+          version = "~> 2.17"
+        }
     }
 }
 
@@ -21,7 +21,7 @@ provider "aws" {
 }
 # provider for AWS
 provider "kubernetes" {
-   config_path = "~/.kube/config"
+  config_path = "~/.kube/config"
   # host                   = data.aws_eks_cluster.eks_cluster.endpoint
   # token                  = data.aws_eks_cluster_auth.token
   # cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.aws_eks_cluster.certificate_authority[0].data)  #base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
