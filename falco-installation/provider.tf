064dc23b4f54d4183ef5dbc8be7564f8a56f3783
Santosh Linux <santosh09@gmail.com>
2025-04-11 11:42:39 +0000

    uncomment help provider

--- falco-installation/provider.tf
+++ falco-installation/provider.tf
@@ -28,11 +28,12 @@ provider "kubernetes" {
   # load_config_file       = false
   
 }
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.eks_cluster.endpoint
-#     token                  = data.aws_eks_cluster_auth.token   #data.aws_eks_cluster_auth.cluster.token
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data) 
-#     # load_config_file       = false
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+    # host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    # token                  = data.aws_eks_cluster_auth.token   #data.aws_eks_cluster_auth.cluster.token
+    # cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data) 
+    # # load_config_file       = false
+  }
+}
