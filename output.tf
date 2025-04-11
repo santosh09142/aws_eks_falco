8ecf36f2db5c515966794613e9ea67b70d349836
Santosh Linux <santosh09@gmail.com>
2025-04-11 10:48:20 +0000

    output modified

--- output.tf
+++ output.tf
@@ -30,3 +30,15 @@ output "eks_cloudwatch_role" {
   description = "description"
   depends_on  = []
 }
+
+output "eks_cluster_name" {
+  value       = aws_eks_cluster.eks_cluster.name
+  sensitive   = false
+  description = "description"
+}
+
+output "eks_cluster_endpoint" {
+  value       = aws_eks_cluster.eks_cluster.endpoint
+  sensitive   = false
+  description = "description"
+}
