88676199b6392e8aca1228595420609f3f85d471
Santosh Linux <santosh09@gmail.com>
2025-04-11 04:18:40 +0000

    added k8s power to terraform falco deployment

--- provider.tf
+++ provider.tf
@@ -1,9 +1,17 @@
 # provider for AWS connect
 terraform {
     required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
+          aws = {
+          source  = "hashicorp/aws"
+          version = "~> 5.0"
+        }
+          kubernetes = {
+          source  = "hashicorp/kubernetes"
+          version = "~> 2.36"
+        }
+          helm = {
+          source  = "hashicorp/helm"
+          version = "~> 3.0"
         }
     }
 }
@@ -12,3 +20,17 @@ provider "aws" {
   region = var.region #"us-east-1"
 }
 # provider for AWS
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+}
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    load_config_file       = false
+  }
+}
