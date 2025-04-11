41e3a09511b476eaf399771948100300a8904917
Santosh Linux <santosh09@gmail.com>
2025-04-11 08:50:38 +0000

    removed helm falco installation to another folder as it required kubernetes to install

--- provider.tf
+++ provider.tf
@@ -5,33 +5,34 @@ terraform {
           source  = "hashicorp/aws"
           version = "~> 5.0"
         }
-          kubernetes = {
-          source  = "hashicorp/kubernetes"
-          version = "~> 2.36"
-        }
-          helm = {
-          source  = "hashicorp/helm"
-          version = "~> 2.17"
-        }
+        #   kubernetes = {
+        #   source  = "hashicorp/kubernetes"
+        #   version = "~> 2.36"
+        # }
+        #   helm = {
+        #   source  = "hashicorp/helm"
+        #   version = "~> 2.17"
+        # }
     }
 }
 
-provider "aws" {
-  region = var.region #"us-east-1"
-}
-# provider for AWS
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  load_config_file       = false
+# moveed to eks/falco-installation/falco_installation.tf
+# provider "aws" {
+#   region = var.region #"us-east-1"
+# }
+# # provider for AWS
+# provider "kubernetes" {
+#   host                   = aws_eks_cluster.eks_cluster.endpoint
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+#   load_config_file       = false
   
-}
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.eks_cluster.endpoint
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    # load_config_file       = false
-  }
-}
+# }
+# provider "helm" {
+#   kubernetes {
+#     host                   = aws_eks_cluster.eks_cluster.endpoint
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+#     # load_config_file       = false
+#   }
+# }
