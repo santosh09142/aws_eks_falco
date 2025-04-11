cb314ec1568cb21677708e4c91aaa1c532e87d0e
Santosh Linux <santosh09@gmail.com>
2025-04-11 05:52:26 +0000

    added new custom falco rules file

--- falco_installation.tf
+++ falco_installation.tf
@@ -19,3 +19,26 @@ resource "helm_release" "falco" {
     })
   ]
 }
+
+resource "helm_release" "falco_custom_rules" {
+  # Uncomment the following line to use the KOPS cluster provider
+  # provider = helm.kops_cluster
+  name             = "falco"
+  repository       = "https://falcosecurity.github.io/charts"
+  chart            = "falco"
+#   version          = "4.14.1"
+  namespace        = "falco"
+  create_namespace = true
+  reuse_values     = true
+  wait             = false
+  recreate_pods    = true
+  timeout          = 600
+
+  values = [
+    templatefile("${path.module}/falco-config/falco-custom-rules.yaml", {
+      # aws_region    = var.region
+    #   sqs_name      = module.sqs_sns_subscription.name
+      # irsa_role_arn = aws_iam_role.eks_cloudwatch_role.arn
+    })
+  ]
+}
