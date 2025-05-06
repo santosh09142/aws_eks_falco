cdca2c0374de205419493deb6a9983c81cc77239
Santosh Linux <santosh09@gmail.com>
2025-05-06 14:43:47 +0000

    change in no of nodes

--- create_eks_cluster.tf
+++ create_eks_cluster.tf
@@ -22,7 +22,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   scaling_config {
     desired_size = 2
     max_size     = 3
-    min_size     = 1
+    min_size     = 2
   }
 
   depends_on = [
